bc290abc3d26bae210dfada18eb735bf121d901e
meil <ics.sora.meil@gmail.com>
2018-05-13 16:24:09 +0900

    add xamarin.forms sample

---
+++ Kamihikouki.Sample.XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Kamihikouki.NETStandard;
+using Kamihikouki.NETStandard.Attributes;
+using Kamihikouki.Sample.NETStandard.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kamihikouki.Sample.XamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var viewModel = new CountViewModel();
+            BindingContext = viewModel;
+            var navigator = new CachedNavigator(null);
+            navigator.MethodInject(this, viewModel);
+        }
+
+        [Navigation(typeof(CountViewModel), nameof(CountViewModel.PushRequest))]
+        public Task PushSubPage(CountViewModel viewModel)
+        {
+            return Navigation.PushModalAsync(new SubPage(viewModel));
+        }
+    }
+}
---
+++ Kamihikouki.Sample.XamarinForms/SubPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Kamihikouki.Sample.NETStandard.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kamihikouki.Sample.XamarinForms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubPage : ContentPage
+    {
+        public SubPage(CountViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
